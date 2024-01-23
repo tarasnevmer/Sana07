fba55d2d42a9ad8d1f9b2b19c2c0a3805716bb2a
Taras Nevmerzhytskyi <kn222_ntv@student.ztu.edu.ua>
2024-01-23 15:20:28 +0200

    Створено та реалізовано похідний клас Computer

---
+++ AbstractnessAndException/Products/Computer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractnessAndException.Products
+{
+    public class Computer: Product
+    {
+        public string VideoCard {get;set;}
+        public string Processor { get;set;}
+        public int AmountOfRAM { get;set;}
+        public int Weight {get; set;}
+        public int SSDVolume { get; set;}
+        public int VideoMemory { get; set;}
+
+        public Computer(string name, decimal price, int count, string videoCard, 
+            string processor, int amountOfRAM, int weight, int sSDVolume, 
+            int videoMemory) : base (name, price, count)
+        {
+            VideoCard = videoCard;
+            Processor = processor;
+            AmountOfRAM = amountOfRAM;
+            Weight = weight;
+            SSDVolume = sSDVolume;
+            VideoMemory = videoMemory;
+        }
+
+        public override string Display()
+        {
+            return $"Назва: {Name}\n" +
+                $"Відеокарта: {VideoCard}\n" +
+                $"Процесор: {Processor}\n" +
+                $"Обсяг оперативної пам'яті: {AmountOfRAM}\n" +
+                $"Вага: {Weight}\n" +
+                $"Обсяг SSD: {SSDVolume}\n" +
+                $"Обсяг відеопам'яті: {VideoMemory}\n" +
+                $"Кількість: {Count}\n" +
+                $"Ціна: {Price}";
+        }
+    }
+}
