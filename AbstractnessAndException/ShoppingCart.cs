64ff4e59b6b0a367169d600e6decf270cd4b3292
Taras Nevmerzhytskyi <kn222_ntv@student.ztu.edu.ua>
2024-01-23 15:45:50 +0200

    Створено та реалізовано клас ShoppingCart

---
+++ AbstractnessAndException/ShoppingCart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using AbstractnessAndException.Products;
+
+namespace AbstractnessAndException
+{
+    public class ShoppingCart
+    {
+        private List<Product> items;
+
+        public ShoppingCart() 
+        {
+            items = new List<Product>();
+        }
+    
+        public void AddToCard(Product product)
+        {
+            items.Add(product);
+        }
+
+        public void DisplayCart()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Display());
+            }
+        }
+
+        public decimal CalculateTotal()
+        {
+            decimal total = 0;
+            foreach (var item in items) 
+            {
+                total += item.Price * item.Count;
+            }
+            return total;
+        }
+
+    }
+}
