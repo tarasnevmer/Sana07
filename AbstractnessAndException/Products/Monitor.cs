87d9b6d9c817fc4c3286e5c0ce2f4f62238af9e2
Taras Nevmerzhytskyi <kn222_ntv@student.ztu.edu.ua>
2024-01-23 13:13:43 +0200

    Створено та реалізовано похідний клас Monitor

---
+++ AbstractnessAndException/Products/Monitor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractnessAndException.Products
+{
+    public class Monitor : Product
+    {
+        public float DisplayDiagonal { get; set; }
+        public string DisplayResolution { get; set; }
+        public string TypeMatrix { get; set; }
+        public int UpdateFrequency { get; set; }
+
+        public Monitor(string name, decimal price, int count, float displayDiagonal, 
+            string displayResolution, string typeMatrix, int updateFrequency)
+            : base (name, price, count)
+        { 
+            DisplayDiagonal = displayDiagonal;
+            DisplayResolution = displayResolution;
+            TypeMatrix = typeMatrix;
+            UpdateFrequency = updateFrequency;
+            
+        }
+
+        public override string Display()
+        {
+            return $"Назва: {Name}\n" +
+                $"Діагональ дисплея: {DisplayDiagonal}\n" +
+                $"Роздільна здатність дисплея: {DisplayResolution}\n" +
+                $"Тип матриці: {TypeMatrix}\n" +
+                $"Частота оновлення: {UpdateFrequency} Гц\n" +
+                $"Кількість: {Count}\n" +
+                $"Ціна: {Price}";
+        }
+
+    }
+}
