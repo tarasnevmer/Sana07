4aeda0d4f3f85090b6a95c58751d43c6cd374e8b
Taras Nevmerzhytskyi <kn222_ntv@student.ztu.edu.ua>
2024-01-23 12:52:46 +0200

    Реалізовано астрактний клас Product

--- AbstractnessAndException/Products/Product.cs
+++ AbstractnessAndException/Products/Product.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace AbstractnessAndException.Products
 {
-    public class Product
+    public abstract class Product
     {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+
+        public Product(string name, decimal price, int count)
+        {
+            Name = name;
+            Price = price;
+            Count = count;
+        }
+
+        public abstract string Display();
     }
 }
